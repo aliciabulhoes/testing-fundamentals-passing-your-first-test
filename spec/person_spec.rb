9c0a7e61b2a02e204558e955534f541268513fbe
aliciabulhoes <aliciabulhoes@gmail.com>
2023-02-09 16:37:21 -0500

    add test for full_name

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,11 +1,16 @@
 require_relative "../lib/person"
 
 describe Person do
+  subject(:person) { described_class.new(first_name: first_name, last_name: last_name) }
+
   describe "#full_name" do
+    subject(:full_name) { person.full_name }
+
+    let(:first_name) { 'Alicia' }
+    let(:last_name) { 'Bulhoes' }
+
     it "returns the first and last names concatenated" do
-      # Write your spec here.
-      # You can delete the pending line.
-      pending "Not written yet"
+      expect(full_name).to eq("#{first_name} #{last_name}")
     end
   end
 end
